745ef7a65a0838e7434ef3caae5a2fc95ef535f9
dev-to2k <72263236+dev-to2k@users.noreply.github.com>
2021-11-26 20:08:07 +0700

    commit

--- src/components/Products/index.jsx
+++ src/components/Products/index.jsx
@@ -1,5 +1,5 @@
 import { Box, Container, Flex, Heading, Spinner, Text } from '@chakra-ui/react';
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import Filter from '../Filter';
 import Pagination from '../Shared/Pagination';
 import ProductItem from './ProductItem';
@@ -7,28 +7,29 @@ import Search from './Search';
 
 const Products = ({ data, isLoading }) => {
   const [state, setState] = useState({
-    products: [],
-    currentPage: 0,
-    productsPerPage: 0,
-    active: 0,
+    currentPage: 1,
+    productsPerPage: 12,
+    active: 1,
+    isFilter: false,
+    filter: [],
   });
 
-  useEffect(() => {
-    setState({
-      products: [...data],
-      currentPage: 1,
-      productsPerPage: 12,
-      active: 1,
-    });
-  }, [data]);
+  const { isFilter, filter, currentPage, productsPerPage, active } = state;
 
-  const { products, currentPage, productsPerPage, active } = state;
+  // Logic for displaying products
+  const indexOfLastProduct = currentPage * productsPerPage;
+  const indexOfFirstProduct = indexOfLastProduct - productsPerPage;
 
-  const [isFilter, setIsFilter] = useState(false);
-  const [filter, setFilter] = useState([]);
+  let currentProducts = data.slice(indexOfFirstProduct, indexOfLastProduct);
+
+  // Logic for displaying page numbers
+  const pageNumbers = [];
+  for (let i = 1; i <= Math.ceil(data.length / productsPerPage); i++) {
+    pageNumbers.push(i);
+  }
 
   const onSwitchCategory = (category) => {
-    const newFilter = products.filter(
+    const newFilter = data.filter(
       (product) => product.category === category.toLowerCase()
     );
     setIsFilter(true);
@@ -38,100 +39,6 @@ const Products = ({ data, isLoading }) => {
       setIsFilter(false);
     }
   };
-
-  const onFilter = (data) => {
-    const { price, rate, category } = data;
-    switch (category) {
-      case 'All':
-        if (price === 'low-to-high') {
-          const newFilter = products.sort((a, b) => a.price - b.price);
-          setState({
-            products: [...newFilter],
-          });
-        }
-        if (price === 'high-to-low') {
-          const newFilter = products.sort((a, b) => b.price - a.price);
-          setState({
-            products: [...newFilter],
-          });
-        }
-        if (rate === 'low-to-high') {
-          const newFilter = products.sort(
-            (a, b) => a.rating.rate - b.rating.rate
-          );
-          setState({
-            products: [...newFilter],
-          });
-        }
-        if (rate === 'high-to-low') {
-          const newFilter = products.sort(
-            (a, b) => b.rating.rate - a.rating.rate
-          );
-          setState({
-            products: [...newFilter],
-          });
-        }
-        break;
-      case "Men's clothing":
-      // eslint-disable-next-line no-fallthrough
-      case "Women's clothing":
-      case 'Jewelery':
-      case 'Electronics':
-      default:
-        if (price === 'low-to-high') {
-          const newFilter = filter.sort((a, b) => a.price - b.price);
-          setFilter([...newFilter]);
-        }
-        if (price === 'high-to-low') {
-          const newFilter = filter.sort((a, b) => b.price - a.price);
-          setFilter([...newFilter]);
-        }
-        if (rate === 'low-to-high') {
-          const newFilter = filter.sort(
-            (a, b) => a.rating.rate - b.rating.rate
-          );
-          setFilter([...newFilter]);
-        }
-        if (rate === 'high-to-low') {
-          const newFilter = filter.sort(
-            (a, b) => b.rating.rate - a.rating.rate
-          );
-          setFilter([...newFilter]);
-        }
-        break;
-    }
-  };
-
-  const onSearch = (search) => {
-    const searchName = search.toLowerCase();
-    const newFilter = products.filter((product) =>
-      product.title.toLowerCase().includes(searchName)
-    );
-    setState({
-      products: [...newFilter],
-    });
-
-    if (search === '') {
-      setState({
-        products: [...data],
-      });
-    }
-  };
-
-  // Logic for displaying products
-  const indexOfLastProduct = currentPage * productsPerPage;
-  const indexOfFirstProduct = indexOfLastProduct - productsPerPage;
-
-  const currentProducts = products.slice(
-    indexOfFirstProduct,
-    indexOfLastProduct
-  );
-
-  // Logic for displaying page numbers
-  const pageNumbers = [];
-  for (let i = 1; i <= Math.ceil(products.length / productsPerPage); i++) {
-    pageNumbers.push(i);
-  }
 
   const handleClick = (e) => {
     setState({
@@ -159,6 +66,70 @@ const Products = ({ data, isLoading }) => {
         active: currentPage + 1,
       });
     }
+  };
+
+  const onFilter = (data) => {
+    const { price, rate, category } = data;
+    switch (category) {
+      case 'All':
+        if (price === 'low-to-high') {
+          [...currentProducts].sort((a, b) => a.price - b.price);
+        }
+        if (price === 'high-to-low') {
+        }
+        if (rate === 'low-to-high') {
+        }
+        if (rate === 'high-to-low') {
+        }
+        break;
+      case "Men's clothing":
+      case "Women's clothing":
+      case 'Jewelery':
+      case 'Electronics':
+      default:
+        if (price === 'low-to-high') {
+          const newFilter = filter.sort((a, b) => a.price - b.price);
+          setState({
+            ...state,
+            filter: [...newFilter],
+          });
+        }
+        if (price === 'high-to-low') {
+          const newFilter = filter.sort((a, b) => b.price - a.price);
+          setState({
+            ...state,
+            filter: [...newFilter],
+          });
+        }
+        if (rate === 'low-to-high') {
+          const newFilter = filter.sort(
+            (a, b) => a.rating.rate - b.rating.rate
+          );
+          setState({
+            ...state,
+            filter: [...newFilter],
+          });
+        }
+        if (rate === 'high-to-low') {
+          const newFilter = filter.sort(
+            (a, b) => b.rating.rate - a.rating.rate
+          );
+          setState({
+            ...state,
+            filter: [...newFilter],
+          });
+        }
+        break;
+    }
+  };
+
+  const onSearch = (search) => {
+    const searchName = search.toLowerCase();
+    const newFilter = data.filter((product) =>
+      product.title.toLowerCase().includes(searchName)
+    );
+    setIsFilter(true);
+    setFilter(newFilter);
   };
 
   const renderProducts = () => {
